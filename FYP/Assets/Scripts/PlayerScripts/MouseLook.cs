ce8086176b6d15a57164ae3c241570cca5792230
MakManKwong <101373859+MakManKwong@users.noreply.github.com>
2022-11-13 22:30:47 +0800

    Update MouseLook script

--- FYP/Assets/Scripts/PlayerScripts/MouseLook.cs
+++ FYP/Assets/Scripts/PlayerScripts/MouseLook.cs
@@ -10,6 +10,9 @@ public class MouseLook : MonoBehaviour
 
     float xRotation = 0f;
 
+    public bool lockCursor = true;
+    private bool m_cursorIsLocked = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +31,47 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        UpdateCursorLock();
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
