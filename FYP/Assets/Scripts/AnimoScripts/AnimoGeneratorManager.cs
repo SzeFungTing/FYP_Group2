34283399777b254676f1c936dfed8b1b08e861d2
PurpleNgui <101373878+PurpleNgui@users.noreply.github.com>
2022-11-14 17:08:12 +0800

    Animo Generator

---
+++ FYP/Assets/Scripts/AnimoScripts/AnimoGeneratorManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimoGeneratorManager : MonoBehaviour
+{
+    int animoCount = 0;
+    bool canGenerate = true;
+    float cooldown = 0.0f;
+
+    [SerializeField] int maxNum = 30;
+    [SerializeField] AnimoGenerator ag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+
+        if (cooldown <= 0)
+        {
+            canGenerate = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"));
+        {
+            if(canGenerate)
+            {
+                animoCount = GameObject.FindGameObjectsWithTag("Target").Length;
+
+                if (animoCount <= maxNum)
+                {
+                    int generateNum = Random.Range(1, 6);
+                    ag.Generate(generateNum);
+                }
+
+                canGenerate = false;
+                cooldown = 300.0f;
+            }
+        }
+        
+        
+    }
+}
