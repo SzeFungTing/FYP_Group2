7bffae832d2a64dec91e92c2d4027216bd3998e3
FuKaMan <210230826@stu.vtc.edu.hk>
2022-12-12 00:00:44 +0800

    Start UI

---
+++ FYP/Assets/Scripts/StartUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUI : MonoBehaviour
+{
+
+    public void HomeScene()
+    {
+        SceneManager.LoadScene("HomeScene");
+    }
+
+
+    public void exitgame()
+    {
+        Application.Quit();
+    }
+
+}
