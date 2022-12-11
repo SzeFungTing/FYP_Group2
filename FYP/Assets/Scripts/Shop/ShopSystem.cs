00df3f69e2d414c5347c154d72c6a90b125c429a
FuKaMan <210230826@stu.vtc.edu.hk>
2022-12-11 20:30:05 +0800

    +shop update

--- FYP/Assets/Scripts/Shop/ShopSystem.cs
+++ FYP/Assets/Scripts/Shop/ShopSystem.cs
@@ -6,25 +6,37 @@ public class ShopSystem : MonoBehaviour
 {
 
     public GameObject drone;
+    public GameObject shopUI;
+
+
   
     // Start is called before the first frame update
     void Start()
     {
         drone.SetActive(false);
+        shopUI.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+
+
         if (Input.GetKeyDown(KeyCode.C))
         {
             
             drone.SetActive(!drone.activeSelf);
+            shopUI.SetActive(!shopUI.activeSelf);
+
+
 
         }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-
+            shopUI.SetActive(false);
             drone.SetActive(false);
 
         }
