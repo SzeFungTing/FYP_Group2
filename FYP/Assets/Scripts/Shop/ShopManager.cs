4db6c6510b4d7c0c053bd2311d6765437c395e92
FuKaMan <210230826@stu.vtc.edu.hk>
2023-01-07 00:16:44 +0800

    +shop system update

--- FYP/Assets/Scripts/Shop/ShopManager.cs
+++ FYP/Assets/Scripts/Shop/ShopManager.cs
@@ -32,9 +32,9 @@ public class ShopManager : MonoBehaviour
         shopItems[1, 4] = 4;
 
         //Price
-        shopItems[2, 1] = 10;
+        shopItems[2, 1] = 30;
         shopItems[2, 2] = 1000; //jetpack
-        shopItems[2, 3] = 30;
+        shopItems[2, 3] = 10;
         shopItems[2, 4] = 40;
 
         //Quantity
@@ -46,7 +46,7 @@ public class ShopManager : MonoBehaviour
         //Limit
         shopItems[4, 1] = 3;
         shopItems[4, 2] = 1;
-        shopItems[4, 3] = 0;
+        shopItems[4, 3] = 10;
         shopItems[4, 4] = 0;
 
 
@@ -80,7 +80,7 @@ public class ShopManager : MonoBehaviour
                 shopItems[4, ButtonRef.GetComponent<ButtonInfo>().ItemID]--;
                 shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID]++;
 
-               Instantiate(ButtonRef.GetComponent<ButtonInfo>().SItem, transform.position,new Quaternion(-0.7071068f, 0,0, 0.7071068f));
+               Instantiate(ButtonRef.GetComponent<ButtonInfo>().SItem, transform.position,Quaternion.identity);
 
                 CoinsTXT.text = "Coins: $" + coins.ToString(); //update the coins player have
                 ButtonRef.GetComponent<ButtonInfo>().QuantityTxt.text = shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID].ToString();
