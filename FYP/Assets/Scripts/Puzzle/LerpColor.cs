8625f5b097b14bc4b4835ef71882050d0077afe0
FuKaMan <210230826@stu.vtc.edu.hk>
2023-02-07 23:32:29 +0800

    +puzzle script update

--- FYP/Assets/Scripts/Puzzle/LerpColor.cs
+++ FYP/Assets/Scripts/Puzzle/LerpColor.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class LerpColor : MonoBehaviour
 {
+    public GameObject hexagon;
+
+    void Start()
+    {
+        //var hexRenderer = GetComponent<Renderer>().material.color = hexagon.GetComponent<Renderer>().material.GetColor("Orange_light");
+        //var hexRenderer = hexagon.GetComponent<Renderer>();
+        //hexRenderer.material.SetColor("_Color", Color.red);
+        //Debug.Log("o");
+
+        Mesh mesh = (gameObject.GetComponent(typeof(MeshFilter)) as MeshFilter).mesh;
+
+        Color[] colours = mesh.colors;
+        colours[1] = Color.red;
+
+        //for (int i = 0; i < colours.Length; i++)
+        //{
+        //    colours[i] = Color.red;//the new colour you want to set it to.
+        //}
+    }
 
 }
