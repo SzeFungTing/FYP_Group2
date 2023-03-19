0e4093155434e1f71ac2d50ade0057640c6d8644
FuKaMan <210230826@stu.vtc.edu.hk>
2023-03-19 19:38:09 +0800

    +Skybox Day night cycle script(ver1)

--- FYP/Assets/Scripts/GameManagerScripts/SkyboxManagerScript.cs
+++ FYP/Assets/Scripts/GameManagerScripts/SkyboxManagerScript.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class SkyboxManagerScript : MonoBehaviour
 {
-    public float skyboxSpeed = 1.0f;
+    public float skyboxSpeed = 1.5f;
+    public Material skyMaterial;
+    public float T=0;
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * skyboxSpeed);
+        T -= Time.deltaTime;
+        if (T<=0)
+            RenderSettings.skybox = skyMaterial;
+
     }
 }
