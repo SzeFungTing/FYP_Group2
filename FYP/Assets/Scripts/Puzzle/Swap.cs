bc2b158d82931246e19aa52ba0409f0e5f3aa7e6
MakManKwong <101373859+MakManKwong@users.noreply.github.com>
2023-05-13 00:53:29 +0800

    Update

--- FYP/Assets/Scripts/Puzzle/Swap.cs
+++ FYP/Assets/Scripts/Puzzle/Swap.cs
@@ -5,9 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class Swap : MonoBehaviour
 {
+    bool HomeScene = false;
+    bool Maze = false;
+    void Start()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        Debug.Log(currentSceneName);
+        if (currentSceneName == "HomeScene")
+        {
+            HomeScene = true;
+        }
+        else if (currentSceneName == "Puzzle1")
+        {
+            Maze = true;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other== other.CompareTag("Player"))
-            SceneManager.LoadScene("Maze");
+        if (other == other.CompareTag("Player"))
+            if (HomeScene)
+            {
+                SceneManager.LoadScene("Puzzle1");
+            }
+            else if (Maze)
+            {
+                SceneManager.LoadScene("HomeScene");
+            }
     }
 }
