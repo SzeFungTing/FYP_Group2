14a893e5b01bbf6b29d47d2625c202b5156545b4
MakManKwong <101373859+MakManKwong@users.noreply.github.com>
2023-05-14 00:28:28 +0800

    Update LoadingUI and portal

--- FYP/Assets/Scripts/Puzzle/Swap.cs
+++ FYP/Assets/Scripts/Puzzle/Swap.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Swap : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider loadingBar;
     bool HomeScene = false;
     bool Maze = false;
     void Start()
     {
         string currentSceneName = SceneManager.GetActiveScene().name;
-        Debug.Log(currentSceneName);
         if (currentSceneName == "HomeScene")
         {
             HomeScene = true;
@@ -26,11 +28,43 @@ public class Swap : MonoBehaviour
         if (other == other.CompareTag("Player"))
             if (HomeScene)
             {
-                SceneManager.LoadScene("Puzzle1");
+                StartCoroutine(LoadSceneAsynchronously(5));
             }
             else if (Maze)
             {
-                SceneManager.LoadScene("HomeScene");
+                StartCoroutine(LoadSceneAsynchronously(0));
             }
+        if (other.gameObject.name == "Portal_DarkForest")
+        {
+            StartCoroutine(LoadSceneAsynchronously(1));
+        }
+        else if (other.gameObject.name == "Portal_Desert")
+        {
+            StartCoroutine(LoadSceneAsynchronously(2));
+        }
+        else if (other.gameObject.name == "Portal_ice")
+        {
+            StartCoroutine(LoadSceneAsynchronously(3));
+        }
+        else if (other.gameObject.name == "Portal_Volcano")
+        {
+            StartCoroutine(LoadSceneAsynchronously(4));
+        }
+        else if (other.gameObject.name == "Portal_Home")
+        {
+            StartCoroutine(LoadSceneAsynchronously(0));
+        }
+    }
+
+    IEnumerator LoadSceneAsynchronously(int levelIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+            loadingBar.value = operation.progress;
+            yield return null;
+        }
     }
 }
